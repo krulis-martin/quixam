a138b000f2c11712ffa8087edcd32233b319d6de
Martin Kruliš <krulis@d3s.mff.cuni.cz>
2026-04-17 23:58:39 +0200

    Adding buttons for test-state manipulation to Enrollment page as well, refactoring the code to avoid duplication.

--- app/Helpers/Latte/FunctionsExtension.php
+++ app/Helpers/Latte/FunctionsExtension.php
@@ -8,8 +8,15 @@ use App\Model\Entity\Answer;
 use App\Model\Entity\Question;
 use Latte\Extension;
 
+/**
+ * Latte extension for functions related to displaying the test results.
+ */
 class FunctionsExtension extends Extension
 {
+    /**
+     * Register functions for Latte templates.
+     * @return array function name => callback
+     */
     public function getFunctions(): array
     {
         // Register methods as Latte functions
@@ -22,12 +29,22 @@ class FunctionsExtension extends Extension
         ];
     }
 
+    /**
+     * Check if the answer has full points (i.e., no mistakes).
+     * @param Answer|null $answer
+     * @return bool true if the answer has full points, false otherwise
+     */
     public function hasAnswerFullPoints(?Answer $answer): bool
     {
         return $answer !== null && $answer->getPoints() !== null
             && $answer->getPoints() >= $answer->getQuestion()->awardPointsForAnswer(0);
     }
 
+    /**
+     * Check if the answer has partial points (i.e., some mistakes but not all wrong nor correct).
+     * @param Answer|null $answer
+     * @return bool true if the answer has only partial points, false otherwise
+     */
     public function hasAnswerPartialPoints(?Answer $answer): bool
     {
         $question = $answer?->getQuestion();
@@ -36,6 +53,11 @@ class FunctionsExtension extends Extension
             && $answer->getPoints() < $question->awardPointsForAnswer(0);
     }
 
+    /**
+     * Check if the answer has no points (i.e., all mistakes or empty answer).
+     * @param Answer|null $answer
+     * @return bool true if the answer has no points, false otherwise
+     */
     public function hasAnswerNoPoints(?Answer $answer): bool
     {
         $question = $answer?->getQuestion();
@@ -43,9 +65,16 @@ class FunctionsExtension extends Extension
             && $answer->getPoints() <= $question->awardPointsForAnswer($question->getItemsCount());
     }
 
+    /**
+     * Determine the CSS class-base for styling (in Bootstrap) based on the points of the answer.
+     * @param Answer|null $answer the answer for which the style should be determined
+     * @param bool $testFinished whether the test is finished (grading is displayed)
+     * @return string|null CSS class base for styling (e.g., 'success', 'warning', 'danger'),
+     *                     null if no special styling should be applied
+     */
     public function answerStyle(?Answer $answer, bool $testFinished = false): ?string
     {
-        if (!$answer) {
+        if (!$answer || (!$testFinished && $answer->getAnswer() === null)) {
             return null;
         }
 
@@ -66,6 +95,11 @@ class FunctionsExtension extends Extension
         }
     }
 
+    /**
+     * Format points for display, showing a range if there are different points for different numbers of mistakes.
+     * @param Question|null $question the question for which the points should be formatted
+     * @return string|null formatted points for display, null if the question is null
+     */
     public function formatPoints(?Question $question): ?string
     {
         if (!$question) {
---
+++ app/Presenters/base/TestHandlingPresenter.php
@@ -0,0 +1,178 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use App\Model\Entity\EnrolledUser;
+use App\Model\Entity\TestTerm;
+use App\Model\Entity\User;
+use App\Model\Repository\EnrolledUsers;
+use App\Model\Repository\EnrollmentRegistrations;
+use App\Model\Repository\TestTerms;
+use App\Helpers\TestOrchestrator;
+use Tracy\ILogger;
+use Exception;
+
+class TestHandlingPresenter extends AuthenticatedPresenter
+{
+    /** @var EnrolledUsers @inject */
+    public $enrolledUsers;
+
+    /** @var EnrollmentRegistrations @inject */
+    public $enrollmentRegistrations;
+
+    /** @var TestTerms @inject */
+    public $testTerms;
+
+    /** @var TestOrchestrator @inject */
+    public $testOrchestrator;
+
+    /** @var ILogger @inject */
+    public $logger;
+
+    /**
+     * Helper function that retrieves test term by its ID.
+     * The invocation is ended by finalize method in case of error.
+     * @param string $id of the the term
+     * @return TestTerm|null on success (null is here only for PHPStan, it will never happen)
+     */
+    protected function getTest(string $id): ?TestTerm
+    {
+        $test = $this->testTerms->get($id);
+        if (!$test || $test->getArchivedAt()) {
+            $this->finalizePostError($this->translator->translate('locale.dashboard.error.invalidId'));
+        }
+        return $test;
+    }
+
+    /**
+     * Helper function that retrieves test term by its ID and verifies the user can modify it.
+     * The invocation is ended by finalize method in case of error.
+     * @param string $id of the the term
+     * @return TestTerm|null on success (null is here only for PHPStan, it will never happen)
+     */
+    protected function getTestForModification(string $id): ?TestTerm
+    {
+        $test = $this->getTest($id);
+        if (
+            $this->user->getRole() === User::ROLE_ADMIN ||
+            ($this->user->getRole() !== User::ROLE_STUDENT && $test->getSupervisors()->contains($this->user))
+        ) {
+            return $test;
+        }
+
+        $this->finalizePostError($this->translator->translate('locale.dashboard.error.notAuthorized'));
+        return null;
+    }
+
+    /*
+     * Signals
+     */
+
+    /**
+     * Start button pressed (test goes from planned -> started).
+     */
+    public function handleStart(string $id)
+    {
+        $test = $this->getTestForModification($id);
+        if ($test->getStartedAt()) {
+            $this->finalizePostError($this->translator->translate('locale.dashboard.error.invalidTestState'));
+        }
+        $test->startNow();
+        $this->testTerms->persist($test);
+        $this->finalizePost($this->link('this'));
+    }
+
+    /**
+     * Finish button pressed (test goes from started -> finished).
+     */
+    public function handleFinish(string $id)
+    {
+        $test = $this->getTestForModification($id);
+        if (!$test->getStartedAt() || $test->getFinishedAt()) {
+            $this->finalizePostError($this->translator->translate('locale.dashboard.error.invalidTestState'));
+        }
+
+        try {
+            $this->testTerms->beginTransaction();
+            $this->testOrchestrator->evaluate($test);
+            $test->finishNow();
+            $this->testTerms->persist($test);
+            $this->testTerms->commit();
+        } catch (Exception $e) {
+            $this->testTerms->rollback();
+            $this->logger->log($e, ILogger::EXCEPTION);
+            $this->finalizePostError($this->translator->translate('locale.dashboard.error.evaluationFailure'));
+        }
+
+        $this->finalizePost($this->link('this'));
+    }
+
+    /**
+     * Revoke start button pressed (test goes from started -> planned).
+     */
+    public function handleRevokeStart(string $id)
+    {
+        $test = $this->getTestForModification($id);
+        if (!$test->getStartedAt() || $test->getFinishedAt()) {
+            $this->finalizePostError($this->translator->translate('locale.dashboard.error.invalidTestState'));
+        }
+        $test->setStartedAt(null);
+        $this->testTerms->persist($test);
+        $this->finalizePost($this->link('this'));
+    }
+
+    /**
+     * Revoke start button pressed (test goes from finished -> started).
+     */
+    public function handleRevokeFinish(string $id)
+    {
+        $test = $this->getTestForModification($id);
+        if (!$test->getFinishedAt()) {
+            $this->finalizePostError($this->translator->translate('locale.dashboard.error.invalidTestState'));
+        }
+        $test->setFinishedAt(null);
+        $this->testTerms->persist($test);
+        $this->finalizePost($this->link('this'));
+    }
+
+    /**
+     * Enroll button pressed, the enrolled user entity is created for registered user.
+     * Also the complete instance of the test is generated at this very moment.
+     */
+    public function handleEnroll(string $id)
+    {
+        $test = $this->getTest($id);
+
+        // if the user is already enrolled -> just redirect
+        if ($this->enrolledUsers->findOneBy(['test' => $id, 'user' => $this->user->getId()]) !== null) {
+            $this->finalizePost($this->link('Test:default', ['id' => $id]));
+        }
+
+        // check the registration
+        $this->enrollmentRegistrations->reassociateUser($this->user);
+        $registration = $this->enrollmentRegistrations->findOneBy(['test' => $id, 'user' => $this->user->getId()]);
+        if (!$registration) {
+            $this->finalizePostError($this->translator->translate('locale.dashboard.error.notRegistered'));
+        }
+
+        try {
+            $this->enrolledUsers->beginTransaction();
+
+            // create enrollment and delete registration
+            $enrolledUser = new EnrolledUser($test, $this->user);
+            $this->enrollmentRegistrations->remove($registration);
+            $this->enrolledUsers->persist($enrolledUser);
+
+            $this->testOrchestrator->instantiate($enrolledUser);
+            $this->enrolledUsers->commit();
+        } catch (Exception $e) {
+            $this->enrolledUsers->rollback();
+            $this->logger->log($e, ILogger::EXCEPTION);
+            $this->finalizePostError($this->translator->translate('locale.dashboard.error.instantiationFailure'));
+        }
+
+        $this->finalizePost($this->link('Test:default', ['id' => $id]));
+    }
+}
